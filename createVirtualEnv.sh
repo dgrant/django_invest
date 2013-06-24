68333518d797d69b8ac5dc20a3f7e0272e71e12c
David Grant <davidgrant@gmail.com>
2013-06-24 14:12:16 -0700

    refactored path locations a bit

--- createVirtualEnv.sh
+++ createVirtualEnv.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 rm -rf env.bak
 mv env env.bak
-sudo apt-get update
-sudo apt-get install python-setuptools libmysqlclient-dev python-dev
 sudo easy_install pip
+sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
-virtualenv --distribute env
+virtualenv --distribute --no-site-packages env
 env/bin/pip install --download-cache=~/.pip-cache -r requirements.txt
